0a3625443e4d6a676497f8d029bf14eea47f62a8
ironarmchad <mcline@muralab.org>
2017-04-19 15:25:09 -0400

    added ability to print imgs and rotate camera.

--- pymolScripts/buildConnectivity.py
+++ pymolScripts/buildConnectivity.py
@@ -1,14 +1,25 @@
 from pymol import cmd
-
-class progroup():
-    def __init__(self, startIt):
-        self.it = startIt
+import numpy as np
         
 
+def scantoview(newview, steps, imgstart):
+    imgval = imgstart
+    oldview = cmd.get_view()
+    delta = np.subtract(newview, oldview)
+    delta = delta / steps
+    for i in range(1,steps):
+        print(imgval)
+        currentview = np.add(oldview, delta * i)
+        cmd.set_view(currentview)
+        cmd.png("img{:04}".format(imgval))
+        imgval += 1
+    return imgval
+    
 class branch():
-    def __init__(self, startIt):
+    def __init__(self, startIt, imgstart):
         #Needed declarations
         self.it = startIt
+        self.imgval = imgstart
         self.startE = -1
         self.rm = -1
         self.rb = -1
@@ -44,6 +55,10 @@ class branch():
             r1, g1, b1 = past[2] * self.rm + self.rb, past[2] * self.gm + self.gb, past[2] * self.bm + self.bb
             cmd.set_color("chold"+str(self.it), [r1, g1, b1])
             
+            #printing img
+            cmd.png("img{:04d}".format(self.imgval))
+            self.imgval += 1
+            
             #Setting up current atom
             if myspace["currentAD"] == []:
                 break
@@ -58,47 +73,45 @@ class branch():
         
             #Making connector
             radius = .4
+            holdview = cmd.get_view()
             cmd.load_cgo([9.0, x1, y1, z1, x2, y2, z2, radius, r1, g1, b1, r2, g2, b2], "c" + str(self.it))    
+            cmd.set_view(holdview)
             self.it += 1
             
-    def buildConnectivity(self, past, current):
-        
-        
-        #Fixing periodic boundaries
-        translate = False
-        
-        '''
-        if x2 - x1 > 4:
-            if x2 > 0:
-                x2 += 1000
-                cmd.translate([1000, 0, 0], 'current')
-            elif x2 < 0:
-                x2 += -1000
-                cmd.translate([-1000, 0, 0], 'current')
-        if y2 - y1 > 4:
-            if y2 > 0:
-                y2 += 1000
-                cmd.translate([0, 1000, 0], 'current')
-            if y2 < 0:
-                y2 += -1000
-                cmd.translate([0, -1000, 0], 'current')
-        '''
-        
-        
-    
-    
-    
-
 def main():
     cmd.load("..\\testfiles\\trackplot.pdb")
     cmd.hide("everything")
     
-    it = 1
-    while it < 1000:
-        
-        holdbranch = branch(it)
-        it = holdbranch.it
-        it += 1
+    it, imgval = 1, 1
+    #Making the movie
+    cmd.set_view((\
+        0.989454925,    0.119257055,    0.082188576,\
+        -0.078681566,    0.919020355,   -0.386279583,\
+        -0.121599913,    0.375739068,    0.918713212,\
+        -0.000024408,    0.000082886, -1595.923706055,\
+        111.845207214,  103.542381287,  915.251525879,\
+        1469.349487305, 1722.521118164,  -20.000000000 ))
+    
+    holdbranch = branch(it, imgval)
+    it = holdbranch.it + 1
+    imgval = holdbranch.imgval
+    holdbranch = branch(it, imgval)
+    it = holdbranch.it + 1
+    imgval = holdbranch.imgval
+    holdbranch = branch(it, imgval)
+    it = holdbranch.it + 1
+    imgval = holdbranch.imgval
+    
+    imgval = scantoview((\
+        0.301146746,    0.683516979,   -0.664916396,\
+        0.748767972,   -0.601271391,   -0.278967589,\
+        -0.590473652,   -0.413857192,   -0.692866027,\
+        -0.000024408,    0.000082886, -268.568328857,\
+        111.845207214,  103.542381287,  915.251525879,\
+        141.994079590,  395.165588379,  -20.000000000 ), 20, imgval)
+    
+    
+
     
     
 if __name__ == "__main__":
