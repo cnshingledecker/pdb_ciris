7c65cc67a8bdd1e87025f78ab8295139ce34a29c
ironarmchad <mcline@muralab.org>
2017-05-02 13:37:24 -0400

    Testing processes, probably ready for pull to master.

--- pymolScripts/buildConnectivity.py
+++ pymolScripts/buildConnectivity.py
@@ -4,16 +4,20 @@ import numpy as np
 #Stores a gradient function for use in coloring
 class Gradient():
     def __init__(self, e1, rgb1, e2, rgb2):
+        self.eMax = e1
         self.slope = np.subtract(rgb2, rgb1) / (e2 - e1)
         self.interc = np.subtract(rgb2, e2 * self.slope)
       
     #Given an energy value, gives a RGB value
     def color(self, energy):
-        return list(self.slope * energy + self.interc)
+        if energy > self.eMax:
+            return [0, 0, 0]
+        else:
+            return list(self.slope * energy + self.interc)
 
 #Class for storing image print settings
 class Pngprinter():
-    def __init__(self, actuallyPrint = True):
+    def __init__(self, actuallyPrint = "Do it"):
         self.filebase = "img"
         self.width = "6in"
         self.height = "6in"
@@ -25,8 +29,10 @@ class Pngprinter():
         
     #Used to print an image
     def out(self):
-        if self.actuallyPrint:
+        if self.actuallyPrint == "Do it":
             cmd.png("{}{:05d}".format(self.filebase, self.imgval), self.width, self.height, self.dpi, self.ray, self.quiet)
+        elif self.actuallyPrint == "Quick":
+            cmd.save("{}{:05d}.png".format(self.filebase, self.imgval))
         self.imgval += 1
   
 #Device for traversing electron path  
@@ -65,87 +71,128 @@ class AtomNode():
     #Makes connector
     def connect(self):
         x1, y1, z1 = self.pastAD[4], self.pastAD[5], self.pastAD[6]
-        rgb1 = self.ECG.color(self.pastAD[3])             
+        rgb1 = self.ECG.color(self.pastAD[3])
         x2, y2, z2 = self.currentAD[4], self.currentAD[5], self.currentAD[6]
         rgb2 = self.ECG.color(self.currentAD[3])
         holdview = cmd.get_view()
         cmd.load_cgo([9.0, x1, y1, z1, x2, y2, z2, self.conRad, rgb1[0], rgb1[1], rgb1[2], rgb2[0], rgb2[1], rgb2[2]], "c" + str(self.it))    
-        cmd.set_view(holdview)
+        cmd.set_view(holdview)        
         
     #method which adds the next atom and connects it 
     def addNext(self):
         if self.currentAD == []:
             return "END"
-        elif self.pastAD[1] != self.currentAD[1]:            
+        elif self.pastAD[1] != self.currentAD[1]:
+            #Finding last proton
+            myspace = {"near": []} 
+            cmd.select("poss", "resn PRO")
+            cmd.iterate("poss", "near.append([ID, resi, q, b])", space=myspace)
+            near = myspace["near"]
+            near.sort(key=lambda x: x[0])
+            for i in range(0, len(near)):
+                if near[i][0] == self.currentAD[0]:
+                    i -= 1
+                    break
+            self.pastAD = self.setdata("past", near[i][0])
+            #Advancing atom
+            self.connect()
             self.advanceAtom()
             return "newProt"
         elif int(round(self.pastAD[2]*100)) != int(round(self.currentAD[2]*100)):
-            myspace = {"near": []}            
+            #Finding highest energy "close" atom
+            myspace = {"near": []}
             cmd.select("poss","current around 13")
             cmd.iterate("poss", "near.append([ID, resi, q, b])", space=myspace)
             near = myspace["near"]
             near.sort(key=lambda x: x[2])
             self.pastAD = self.setdata("past", near[0][0])
-            self.connect()            
+            #Advancing atom
+            self.connect()
             self.advanceAtom()
             return "newBranch"
         else:
             self.connect()
             self.advanceAtom()
             return None
+            
+    #Adds the next whole branch
+    def addNextBranch(self, pngprinter):
+        breaker = None
+        while(breaker == None):
+            breaker = self.addNext()
+            pngprinter.out()
+        return breaker
+        
+    def addNextAtoms(self, pngprinter, start, stop):
+        for i in range(start, stop):
+            self.addNext()
+            pngprinter.out()
 
 #Linearly shifts view to a new point. Give it the new view matrix, the Pngprinter, the number of imgs to print and the number the img starts at.
 def scantoview(newview, steps, pngprinter):
     oldview = cmd.get_view()
     delta = np.subtract(newview, oldview)
     delta = delta / steps
-    for i in range(0, steps):
+    for i in range(0, steps + 1):
         currentview = np.add(oldview, delta * i)
         cmd.set_view(currentview)
         pngprinter.out()
         
 def main():
     #Load in structure and set stage
-    cmd.load("..\\testfiles\\trackplot.pdb")
+    cmd.load("..\\testfiles\\trackplotClean.pdb")
     cmd.hide("everything")
     cmd.bg_color("white")
     cmd.clip("slab", 1000)
     
-    #Sets up energy color gradient (this can be changed at any time)
-    energyColor = Gradient(86.56, (1, 0 , 0), 0, (.70, .70, .70))
+    #Creates important objects: atom node and pngprinter
+    atomInfo = AtomNode(Gradient(35, (1, 0 , 0), 0, (.70, .70, .70)))
+    pngprinter = Pngprinter("Quick")
     
+    #Setting up for quick prints
+    #pngprinter.ray = 0
+    #pngprinter.dpi = 100
+    
+    #Creates the movie
     cmd.set_view((\
-        0.957694948,    0.028183326,   -0.286400944,\
-        -0.138612419,    0.917322457,   -0.373235583,\
-        0.252203047,    0.397143424,    0.882423639,\
-        -0.000182331,   -0.003953297, -487.787475586,\
-        -71.356201172,  -66.460510254, 3058.803955078,\
-        -501.575195312, 1478.104492188,  -20.000000000 ))
-    
-    atomInfo = AtomNode(energyColor)
-    pngprinter = Pngprinter()
-    for i in range(0,69):
-        atomInfo.addNext()
-        pngprinter.out()
+        0.659889638,    0.127799481,    0.740413308,\
+        -0.385417193,    0.903480828,    0.187554732,\
+        -0.644981921,   -0.409131795,    0.645453334,\
+        -0.006196454,    0.004652441, -788.275756836,\
+        63.789550781,  -30.806030273, 2916.111083984,\
+        -32534.138671875, 34107.878906250,  -20.000000000 ))
+        
+    atomInfo.addNextAtoms(pngprinter, 1, 117)
     
     scantoview((\
-        0.963438809,    0.006933183,   -0.267837793,\
-        -0.117464207,    0.909400284,   -0.398989260,\
-        0.240805432,    0.415861994,    0.876967907,\
-        -0.000037957,   -0.003030211, -446.935913086,\
-        -170.208068848, -159.757263184, 2987.006591797,\
-        -541.689941406, 1437.990112305,  -20.000000000 ), 5, pngprinter)
-        
-    for i in range(69, 109):
-        atomInfo.addNext()
-        pngprinter.out()
+        0.659820437,    0.127603829,    0.740508616,\
+        -0.385310411,    0.903510094,    0.187633067,\
+        -0.645116448,   -0.409128159,    0.645321131,\
+        -0.006378829,    0.004803181, -1291.816406250,\
+        -1.017456055,    1.457458496, 2915.541503906,\
+        -32029.906250000, 34612.128906250,  -20.000000000 ), 5, pngprinter)
     
-    #This bit of code can be used to grab a full branch
+    atomInfo.addNextAtoms(pngprinter, 117, 754)
     
-    breaker = None
-    while(breaker == None):
-        breaker = atomInfo.addNext()
-    print(breaker)
+    scantoview((\
+        0.659625113,    0.135221750,    0.739315271,\
+        -0.385568261,    0.905246913,    0.178425491,\
+        -0.645161927,   -0.402749211,    0.649261534,\
+        -0.008741498,    0.046658099, -1614.154052734,\
+        188.652221680, -151.970001221, 2749.275390625,\
+        -31419.833984375, 34638.425781250,  -20.000000000 ), 5, pngprinter)
+    
+    #cmd.hide("everything")
+    atomInfo.addNextAtoms(pngprinter, 754, 1178)
+    
+    
+    #print(atomInfo.addNextBranch(pngprinter))
+    #pngprinter.actuallyPrint = "Do it"
+    #pngprinter.out()    
+    print(atomInfo.pastAD)
+    print(atomInfo.currentAD)
+    
+    
     
         
     
